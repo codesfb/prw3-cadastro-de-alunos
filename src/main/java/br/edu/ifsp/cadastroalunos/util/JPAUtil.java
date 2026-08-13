bda093d61f86cc7377868245be8a28bfadcc430e
codesfb <fabio.santana950@gmail.com>
2026-08-13 13:30:14 -0300

    fix:desativando logs chatos

--- src/main/java/br/edu/ifsp/cadastroalunos/util/JPAUtil.java
+++ src/main/java/br/edu/ifsp/cadastroalunos/util/JPAUtil.java
@@ -4,9 +4,18 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JPAUtil {
-    private static final EntityManagerFactory FACTORY =
-            Persistence.createEntityManagerFactory("alunos");
+    private static final EntityManagerFactory FACTORY;
+
+        static {
+        // Desativa os logs de INFO e WARN do Hibernate no console
+        Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+
+        FACTORY = Persistence.createEntityManagerFactory("alunos");
+    }
 
     public static EntityManager getEntityManager() {
         return FACTORY.createEntityManager();
