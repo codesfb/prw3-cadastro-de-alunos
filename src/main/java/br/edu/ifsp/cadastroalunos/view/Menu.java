8779a38c2a5facc8f98cd6c46d892f37316420d5
codesfb <fabio.santana950@gmail.com>
2026-08-13 11:33:12 -0300

    feat: menu

--- src/main/java/br/edu/ifsp/cadastroalunos/view/Menu.java
+++ src/main/java/br/edu/ifsp/cadastroalunos/view/Menu.java
@@ -1,5 +1,15 @@
 package br.edu.ifsp.cadastroalunos.view;
 
 public class Menu {
-    // TODO: Implement menu
+    public static void  exibir(){
+        System.out.println("""
+                ** CADASTRO DE ALUNOS **
+                1 - Cadastrar aluno
+                2 - Excluir aluno
+                3 - Alterar aluno
+                4 - Buscar aluno pelo nome
+                5 - Listar alunos (com status de aprovação)
+                6 - FIM
+                """);
+    }
 }
