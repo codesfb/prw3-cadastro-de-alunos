075163c395c9c95f1fd632e862523acd43e22d8f
codesfb <fabio.santana950@gmail.com>
2026-08-13 08:23:42 -0300

    feat: adcionando factory de persistencia

--- src/main/java/br/edu/ifsp/cadastroalunos/util/JPAUtil.java
+++ src/main/java/br/edu/ifsp/cadastroalunos/util/JPAUtil.java
@@ -1,5 +1,15 @@
 package br.edu.ifsp.cadastroalunos.util;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JPAUtil {
-    // TODO: Implement JPA util
+    private static final EntityManagerFactory FACTORY =
+            Persistence.createEntityManagerFactory("aluno ");
+
+    public static EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
+    }
+
 }
